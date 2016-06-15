bdaf2f1d0970839b0929d34723e7eb9bb797a2c9
F839-018 M <MF839-018@70-12-244-238.pools.spcsdns.net>
2016-06-15 16:53:17 +0900

    Initial Commit

---
+++ Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by MF839-018 on 2016. 6. 15..
+//  Copyright © 2016년 MF839-018. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
